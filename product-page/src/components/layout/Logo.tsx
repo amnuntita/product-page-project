23fd94bde9be86a93d748a47c6755313cf3027be
Nuntita Taratchon <6130286921@student.chula.ac.th>
2022-05-20 17:46:31 +0700

    feat: logo component

---
+++ product-page/src/components/layout/Logo.tsx
@@ -0,0 +1,12 @@
+import { Image } from "@chakra-ui/image";
+import { Box } from "@chakra-ui/layout";
+
+const Logo = () => {
+  return (
+    <Box>
+      <Image src="/images/logo.svg" />
+    </Box>
+  );
+};
+
+export default Logo;
