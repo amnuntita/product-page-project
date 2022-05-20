3bcb6fc6a83a57d043f803c12767b60ff23ad851
Nuntita Taratchon <6130286921@student.chula.ac.th>
2022-05-20 17:46:20 +0700

    feat: top bar

--- product-page/src/components/layout/Topbar.tsx
+++ product-page/src/components/layout/Topbar.tsx
@@ -1,7 +1,42 @@
-import { HStack } from "@chakra-ui/react";
+import { HStack, Divider, Text, Box } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/image";
+import { Avatar } from "@chakra-ui/avatar";
+
+import Logo from "./Logo";
 
 const Topbar = () => {
-  return <HStack w="100%" h="20" bgColor="teal"></HStack>;
+  const renderNavLink = () => {
+    return (
+      <HStack spacing="8">
+        <Text>Collections</Text>
+        <Text>Men</Text>
+        <Text>Women</Text>
+        <Text>About</Text>
+        <Text>Contact</Text>
+      </HStack>
+    );
+  };
+
+  const renderUserMenu = () => {
+    return (
+      <HStack h="100%" alignItems="center" spacing="6">
+        <Image src="/images/icon-cart.svg" />
+        <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
+      </HStack>
+    );
+  };
+
+  return (
+    <>
+      <HStack w="100%" h="10" alignItems="start" spacing="20">
+        <Logo />
+        {renderNavLink()}
+        <Box flexGrow="1" />
+        {renderUserMenu()}
+      </HStack>
+      <Divider />
+    </>
+  );
 };
 
 export default Topbar;
