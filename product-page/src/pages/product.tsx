ccc6b27423561a879cfbfa71a6997e0421f5feb6
Nuntita Taratchon <6130286921@student.chula.ac.th>
2022-05-21 21:12:26 +0700

    feat: data props

--- product-page/src/pages/product.tsx
+++ product-page/src/pages/product.tsx
@@ -9,6 +9,7 @@ import ProductDetail from "../components/ProductDetail";
 import Lightbox from "../components/Lightbox";
 
 import useLightbox from "../hooks/useLightbox";
+import { useCart } from "../context/Cart";
 
 const images = [
   "/images/image-product-1.jpg",
@@ -17,16 +18,25 @@ const images = [
   "/images/image-product-4.jpg",
 ];
 
+const productDetail = {
+  seller: " Sneaker Company",
+  productID: "1",
+  productName: "Fall Limited Edition Sneaker",
+  price: 250.0,
+  discount: 0.5,
+};
+
 const ProductPage = () => {
   const { isShowing, open, close, imageList } = useLightbox();
+
   return (
     <>
       <Container>
         <Content>
-          <VStack w="100%" h="70vh" alignContent="center">
+          <VStack w="100%" h={["100vh", "70vh"]} alignContent="center">
             <Gallery imageList={images} openLightbox={open} />
           </VStack>
-          <ProductDetail />
+          <ProductDetail {...productDetail} thumbnail={images[0]} />
         </Content>
       </Container>
       <Lightbox isShowing={isShowing} close={close} imageList={imageList} />
