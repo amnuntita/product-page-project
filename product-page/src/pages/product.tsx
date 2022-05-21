062cd44a18305563c799c9eb816ea8005ff29d06
Nuntita Taratchon <6130286921@student.chula.ac.th>
2022-05-21 13:55:39 +0700

    feat: product page with lightbox

--- product-page/src/pages/product.tsx
+++ product-page/src/pages/product.tsx
@@ -1,16 +1,36 @@
+import { useState } from "react";
+
 import { VStack } from "@chakra-ui/layout";
 
 import Container from "../components/layout/Container";
 import Content from "../components/layout/Content";
+import Gallery from "../components/Gallery";
+import ProductDetail from "../components/ProductDetail";
+import Lightbox from "../components/Lightbox";
+
+import useLightbox from "../hooks/useLightbox";
+
+const images = [
+  "/images/image-product-1.jpg",
+  "/images/image-product-2.jpg",
+  "/images/image-product-3.jpg",
+  "/images/image-product-4.jpg",
+];
 
 const ProductPage = () => {
+  const { isShowing, open, close, imageList } = useLightbox();
   return (
-    <Container>
-      <Content>
-        <VStack w="100%" h="70vh" />
-        <VStack w="100%" h="70vh" />
-      </Content>
-    </Container>
+    <>
+      <Container>
+        <Content>
+          <VStack w="100%" h="70vh" alignContent="center">
+            <Gallery imageList={images} openLightbox={open} />
+          </VStack>
+          <ProductDetail />
+        </Content>
+      </Container>
+      <Lightbox isShowing={isShowing} close={close} imageList={imageList} />
+    </>
   );
 };
 
