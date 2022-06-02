7e1df6dddbe66c926d9483bfbf837af90b0bb19e
Nuntita Taratchon <6130286921@student.chula.ac.th>
2022-06-02 20:37:10 +0700

    style: responsive on mobile

--- product-page/src/pages/product.tsx
+++ product-page/src/pages/product.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import { VStack } from "@chakra-ui/layout";
+import { useMediaQuery } from "@chakra-ui/react";
 
 import Container from "../components/layout/Container";
 import Content from "../components/layout/Content";
@@ -9,7 +10,6 @@ import ProductDetail from "../components/ProductDetail";
 import Lightbox from "../components/Lightbox";
 
 import useLightbox from "../hooks/useLightbox";
-import { useCart } from "../context/Cart";
 
 const images = [
   "/images/image-product-1.jpg",
@@ -28,13 +28,18 @@ const productDetail = {
 
 const ProductPage = () => {
   const { isShowing, open, close, imageList } = useLightbox();
+  const [isMobile] = useMediaQuery("(max-width: 500px)");
 
   return (
     <>
       <Container>
         <Content>
-          <VStack w="100%" h={["100vh", "70vh"]} alignContent="center">
-            <Gallery imageList={images} openLightbox={open} />
+          <VStack w="100%" h={["auto", "70vh"]} alignContent="center">
+            {isMobile ? (
+              <Gallery imageList={images} showArrow={true} />
+            ) : (
+              <Gallery imageList={images} openLightbox={open} />
+            )}
           </VStack>
           <ProductDetail {...productDetail} thumbnail={images[0]} />
         </Content>
